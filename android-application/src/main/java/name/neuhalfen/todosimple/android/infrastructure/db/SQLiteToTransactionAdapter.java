db391fe770d98af2c34555988764ae6e993f2f49
Jens Neuhalfen <jens@neuhalfen.name>
2014-04-30 10:52:55 +0200

    Made TX state handling more explicit.

--- android-application/src/main/java/name/neuhalfen/todosimple/android/infrastructure/db/SQLiteToTransactionAdapter.java
+++ android-application/src/main/java/name/neuhalfen/todosimple/android/infrastructure/db/SQLiteToTransactionAdapter.java
@@ -16,6 +16,30 @@ public class SQLiteToTransactionAdapter implements Transaction {
 
     private SQLiteDatabase db;
 
+    private static enum TX_STATE {
+        OPEN {
+            @Override
+            public boolean isTxInClosedState() {
+                return false;
+            }
+        }, COMMITTED {
+            @Override
+            public boolean isTxInClosedState() {
+                return true;
+            }
+        }, ABORTED {
+            @Override
+            public boolean isTxInClosedState() {
+                return true;
+            }
+        };
+
+
+        public abstract boolean isTxInClosedState();
+    }
+
+    private TX_STATE state = TX_STATE.COMMITTED;
+
     public SQLiteDatabase getDb() {
         return db;
     }
@@ -23,22 +47,35 @@ public class SQLiteToTransactionAdapter implements Transaction {
     @Override
     public void beginTransaction() {
         Log.i("TX", "beginTransaction");
+        if (!state.isTxInClosedState()) {
+            throw new IllegalStateException("beginTransaction: Transaction is in non-closed state:" + state);
+        }
         db = sqllite.getWritableDatabase();
         db.beginTransaction();
+        state = TX_STATE.OPEN;
     }
+
 
     @Override
     public void commit() throws TransactionRollbackException {
         Log.i("TX", "commit");
+        if (state.isTxInClosedState()) {
+            throw new IllegalStateException("commit: Transaction is in closed state:" + state);
+        }
         db.setTransactionSuccessful();
         db.endTransaction();
+        state = TX_STATE.COMMITTED;
         db = null;
     }
 
     @Override
     public void rollback() {
         Log.i("TX", "rollback");
+        if (state.isTxInClosedState()) {
+            throw new IllegalStateException("commit: Transaction is in closed state:" + state);
+        }
         db.endTransaction();
+        state = TX_STATE.ABORTED;
         db = null;
     }
 }
