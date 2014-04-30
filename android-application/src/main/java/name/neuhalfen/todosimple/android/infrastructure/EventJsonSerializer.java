cf7cd2a0c4b0a4330951c7f292cc63b0b5b56206
Jens Neuhalfen <jens@neuhalfen.name>
2014-04-30 17:39:46 +0200

    green test. If it would compile...

--- android-application/src/main/java/name/neuhalfen/todosimple/android/infrastructure/EventJsonSerializer.java
+++ android-application/src/main/java/name/neuhalfen/todosimple/android/infrastructure/EventJsonSerializer.java
@@ -2,12 +2,16 @@ package name.neuhalfen.todosimple.android.infrastructure;
 
 import name.neuhalfen.myscala.domain.model.Event;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 public class EventJsonSerializer {
     public Event parseEvent(String eventJson) {
+        checkNotNull(eventJson,"json must not be null");
         return null;
     }
 
     public String serializeEvent(Event event) {
+        checkNotNull(event,"event must not be null");
         return null;
     }
 }
