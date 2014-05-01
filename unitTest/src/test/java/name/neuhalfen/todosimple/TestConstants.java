f7384dd9779cd28823dcdd329e3bc79a089ea6fa
Jens Neuhalfen <jens@neuhalfen.name>
2014-05-01 10:43:20 +0200

    Refactoring: extract common test constants

---
+++ unitTest/src/test/java/name/neuhalfen/todosimple/TestConstants.java
@@ -0,0 +1,12 @@
+package name.neuhalfen.todosimple;
+
+import java.util.UUID;
+
+public final class TestConstants {
+    public final static UUID uuid1 = UUID.fromString("11111111-1111-1111-1111-111111111111");
+    public final static UUID uuid2 = UUID.fromString("22222222-2222-2222-2222-222222222222");
+
+
+    private TestConstants() {
+    }
+}
