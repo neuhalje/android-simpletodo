49d1783ebf866ad1380df698e563d15fc190c24b
Jens Neuhalfen <jens@neuhalfen.name>
2014-05-03 15:29:53 +0200

    Move DI module into DI package

--- android-application/src/main/java/name/neuhalfen/todosimple/android/view/TodoViewModule.java
+++ android-application/src/main/java/name/neuhalfen/todosimple/android/view/TodoViewModule.java
@@ -1,11 +1,11 @@
 package name.neuhalfen.todosimple.android.view;
 
 import dagger.Module;
-import name.neuhalfen.todosimple.android.AndroidApplicationModule;
+import name.neuhalfen.todosimple.android.di.AndroidApplicationModule;
 
 @Module(
         injects = {
-                TodoListFragment.class,   TodoDetailFragment.class,
+                TodoListFragment.class, TodoDetailFragment.class,
                 TodoListActivity.class, TodoDetailActivity.class
         },
         addsTo = AndroidApplicationModule.class,
