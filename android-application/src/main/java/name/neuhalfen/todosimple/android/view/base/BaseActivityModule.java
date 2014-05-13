d403e63d0791bc54060f85bdee40fdd47e11063b
Jens Neuhalfen <jens@neuhalfen.name>
2014-05-13 23:28:37 +0200

    Including the EventBusModule twice (in BaseActivityModule & AndroidApplicationModule) creates TWO scopes for singletons. Observed effect: Events published from the backend (AndroidEventPublisher) were not visible in the fronted because they used different EventBus instances.

--- android-application/src/main/java/name/neuhalfen/todosimple/android/view/base/BaseActivityModule.java
+++ android-application/src/main/java/name/neuhalfen/todosimple/android/view/base/BaseActivityModule.java
@@ -2,8 +2,7 @@ package name.neuhalfen.todosimple.android.view.base;
 
 import dagger.Module;
 import name.neuhalfen.todosimple.android.di.AndroidApplicationModule;
-import name.neuhalfen.todosimple.android.di.EventBusModule;
 
-@Module(injects = BaseActivity.class, includes = {EventBusModule.class, ActionBarModule.class}, addsTo = AndroidApplicationModule.class)
+@Module(injects = BaseActivity.class, includes = { ActionBarModule.class}, addsTo = AndroidApplicationModule.class)
 public class BaseActivityModule {
 }
