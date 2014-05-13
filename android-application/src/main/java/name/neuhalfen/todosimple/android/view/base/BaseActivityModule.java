0f5c6886f6bd887bc23fffcf4a0bf2becc3536a3
Jens Neuhalfen <jens@neuhalfen.name>
2014-05-13 23:09:43 +0200

    dagger: refactor modules bc. AndroidEventPublisher gets a different bus than e.g. BaseActivity. Still an open issue.

--- android-application/src/main/java/name/neuhalfen/todosimple/android/view/base/BaseActivityModule.java
+++ android-application/src/main/java/name/neuhalfen/todosimple/android/view/base/BaseActivityModule.java
@@ -1,8 +1,9 @@
 package name.neuhalfen.todosimple.android.view.base;
 
 import dagger.Module;
+import name.neuhalfen.todosimple.android.di.AndroidApplicationModule;
 import name.neuhalfen.todosimple.android.di.EventBusModule;
 
-@Module(injects = BaseActivity.class, includes = {EventBusModule.class, ActionBarModule.class})
+@Module(injects = BaseActivity.class, includes = {EventBusModule.class, ActionBarModule.class}, addsTo = AndroidApplicationModule.class)
 public class BaseActivityModule {
 }
