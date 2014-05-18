a89dc01b675cae21ad078748d48af136c92f3d33
Jens Neuhalfen <jens@neuhalfen.name>
2014-05-18 20:02:31 +0200

    Fix bug in tests: timeformat was wrong.

--- unitTest/src/test/groovy/name/neuhalfen/todosimple/helper/TestConstants.groovy
+++ unitTest/src/test/groovy/name/neuhalfen/todosimple/helper/TestConstants.groovy
@@ -25,9 +25,9 @@ class TestConstants {
     public final static UUID uuid1 = UUID.fromString("11111111-1111-1111-1111-111111111111");
     public final static UUID uuid2 = UUID.fromString("22222222-2222-2222-2222-222222222222");
     public final
-    static DateTime TIME_NOW = DateTime.parse("20140517'T'165800Z", ISODateTimeFormat.basicDateTimeNoMillis())
+    static DateTime TIME_NOW = DateTime.parse("20140517T165800Z", ISODateTimeFormat.basicDateTimeNoMillis())
     public final
-    static DateTime TIME_BEFORE = DateTime.parse("19990517'T'165800Z", ISODateTimeFormat.basicDateTimeNoMillis())
+    static DateTime TIME_BEFORE = DateTime.parse("19990517T165800Z", ISODateTimeFormat.basicDateTimeNoMillis())
     public final
-    static DateTime TIME_AFTER = DateTime.parse("30140517'T'165800Z", ISODateTimeFormat.basicDateTimeNoMillis())
+    static DateTime TIME_AFTER = DateTime.parse("30140517T165800Z", ISODateTimeFormat.basicDateTimeNoMillis())
 }
