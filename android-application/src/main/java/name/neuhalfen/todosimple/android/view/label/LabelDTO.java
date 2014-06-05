ce216a1636ca7fa48baa924e18441e8ad9a22efa
Jens Neuhalfen <jens@neuhalfen.name>
2014-06-05 07:57:00 +0200

    equals/hashCode for LabelDTOs

--- android-application/src/main/java/name/neuhalfen/todosimple/android/view/label/LabelDTO.java
+++ android-application/src/main/java/name/neuhalfen/todosimple/android/view/label/LabelDTO.java
@@ -11,6 +11,22 @@ public class LabelDTO {
         this.name = name;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        LabelDTO labelDTO = (LabelDTO) o;
+
+        if (id != null ? !id.equals(labelDTO.id) : labelDTO.id != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 
     public String toString() {
         return name;
